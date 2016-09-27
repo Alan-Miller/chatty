4605b5ce9f3e5c53f8f3a3f98c640bf60c0f0192
Alan-Miller <alan.ashman@gmail.com>
2016-09-26 21:46:07 -0600

    try2

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
+var messages = ['pumpkin', 'sushi', 'gelatto'];
 
 var app = express();
 
+app.use(cors());
+app.use(bodyParser());
 
+app.get('/', function(req, res) {
+  res.json(messages);
+});
 
+app.post('/', function(req, res) {
+  messages.push(req.body.message);
+  console.log(req.body);
+});
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
+app.listen(8000, function() {
+  console.log('Listening on port 8000');
 });
