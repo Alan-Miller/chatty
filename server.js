e6d51f20275d197c7f68086713a3e0fcf86551af
Alan-Miller <alan.ashman@gmail.com>
2016-09-26 21:25:03 -0600

    try2

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
+var app = express();
+
+
+
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
