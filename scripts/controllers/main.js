7cce7a84d18e8007affbf95500a3060e790c9433
Alan-Miller <alan.ashman@gmail.com>
2017-06-03 13:18:08 -0600

    copied finished project from DM13

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,15 +1,15 @@
-'use strict';
+// 'use strict';
 
 angular.module('chattyApp')
   .controller('MainCtrl', function ( $scope, messageService ) {
     messageService.getMessages().then(function ( response ) {
-      $scope.messages = response.data.messages;
+      $scope.messages = response.data;
     });
 
     $scope.addMessage = function ( message ) {
       if (message) {
         messageService.addMessage(message).then(function ( response ) {
-          $scope.messages = response.data.messages;
+          $scope.messages = response.data;
         });
       }
     };
