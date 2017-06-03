edfc370eba5caff93376ce79e85e265a26a495dd
Alan-Miller <alan.ashman@gmail.com>
2017-06-03 13:20:03 -0600

    changes

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var messages = ['pumpkin', 'sushi', 'gelatto'];
-
 var app = express();
 
-app.use(cors());
-app.use(bodyParser());
 
-app.get('/', function(req, res) {
-  res.json(messages);
-});
 
-app.post('/', function(req, res) {
-  messages.push(req.body.message);
-  console.log(req.body);
-});
 
-app.listen(8000, function() {
-  console.log('Listening on port 8000');
+
+
+app.listen(9000, function() {
+  console.log('I can hear you on port 9000');
 });
